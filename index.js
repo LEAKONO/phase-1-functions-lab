e198bfea4beffa4e126c05440b8199223176b4be
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-03-19 22:30:24 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  const headquaters = 42;
+  if (blocks > 42) {
+      return blocks - headquaters;
+  }else {
+      return headquaters-blocks;
+  }
+}
+function distanceFromHqInFeet(blocks) {
+  const headquaters = 42;
+  if(blocks > 42){
+      return (blocks - headquaters) * 264;
+  }
+  else if (blocks < 42) {
+      return (headquaters - blocks) * 264;
+  }
+}
+
+function distanceTravelledInFeet(a,b){
+  if (b>a){
+    return((b-a)*264);
+  }
+  else
+    return((a-b)*264);
+}
+
+ 
+function calculatesFarePrice(start,destination){
+  let distance = distanceTravelledInFeet(start,destination);
+  if (distance<= 400){
+    return 0;
+  }
+  if (distance>=400 && distance<=2000){
+    return (distance-400)* 0.02;
+  }
+  else if(distance>=2000&& distance<=2500){
+    return 25;
+  }
+  else if (distance > 2500){
+    return "cannot travel that far";
+  }
+}
